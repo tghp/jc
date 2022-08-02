85ed05a4b81a01bdd7c3861e571fea3b191d3475
Chris Grace <chris.grace@tghp.co.uk>
2022-08-02 17:44:36 +0100

    404 page styling

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,54 +1,29 @@
-import * as React from "react"
+import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout";
+import LinkArrow from "../assets/link-arrow.svg";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    return (
+        <Layout location={'error-404'}>
+            <div className="archive-posts">
+                <div className="archive-posts__header">
+                    <div className="archive-posts__header-inner">
+                        <Link to={'/'} className="archive-posts__header-back-btn">
+                            <LinkArrow /> Back to homepage
+                        </Link>
+                        <h1 className="archive-posts__header-title">
+                            Page not found
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div className="default-page-content">
+                <h3>Sorry we couldn’t find what you were looking for</h3>
+                <Link to="/"><LinkArrow /> Go home</Link>
+            </div>
+        </Layout>
+    )
 }
 
 export default NotFoundPage
