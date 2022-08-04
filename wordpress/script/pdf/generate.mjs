b8a57465e16bc0f55d86f7081b81259b15eb7df3
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-08-04 13:07:46 +0100

    Corrected authorisation header format

--- wordpress/script/pdf/generate.mjs
+++ wordpress/script/pdf/generate.mjs
@@ -86,7 +86,7 @@ const triggerModificationForPost = async (postId) => {
             }
         },
         {
-            authorization: `Bearer ${process.env.WP_GRAPHQL_TOKEN}`,
+            authorization: `Basic ${process.env.WP_GRAPHQL_TOKEN}`,
         }
     )
 }
