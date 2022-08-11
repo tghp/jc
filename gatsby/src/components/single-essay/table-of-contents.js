e18170fdcc93139b09e3e5b037d61bd14666ad12
Chris Grace <chris.grace@tghp.co.uk>
2022-08-11 15:36:38 +0100

    Single post: hide toc menu when there is no toc

--- gatsby/src/components/single-essay/table-of-contents.js
+++ gatsby/src/components/single-essay/table-of-contents.js
@@ -5,19 +5,24 @@ const TableOfContents = ({ navItems }) => {
     const [toggleNav, setToggleNav] = useState(true)
     const handleToggle = () => setToggleNav(!toggleNav)
     const buttonText = toggleNav ? 'hide' : 'show'
+    console.log(navItems.length)
 
     return (
-        <div className="single-essay__sidebar-menu">
-            <div className="single-essay__sidebar-menu-title">
-                Contents <button
-                    onClick={handleToggle}
-                    className="single-essay__sidebar-menu-toggle"
-                    type="button">
-                    {buttonText}
-                </button>
+        <>
+        {navItems?.length > 0 &&
+            <div className="single-essay__sidebar-menu">
+                <div className="single-essay__sidebar-menu-title">
+                    Contents <button
+                        onClick={handleToggle}
+                        className="single-essay__sidebar-menu-toggle"
+                        type="button">
+                        {buttonText}
+                    </button>
+                </div>
+                {toggleNav && <TocNav navItems={navItems} />}
             </div>
-            {navItems && toggleNav && <TocNav navItems={navItems} />}
-        </div>
+        }
+        </>
     )
 }
 
