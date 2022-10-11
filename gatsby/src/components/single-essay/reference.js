b4f29903ab419dcdcd7596ca84bf52b7e6c70f0d
Chris Grace <chris.grace@tghp.co.uk>
2022-10-11 13:39:59 +0100

    Refactor useState exports

--- gatsby/src/components/single-essay/reference.js
+++ gatsby/src/components/single-essay/reference.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 
 const Reference = ({ reference, index, referenceSidebarRefs, openIndexes, toggleOpenIndex, clearOpenIndexes }) => {
     let shortenedText
